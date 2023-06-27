035ff9bd5b2503520fdd307df13254165d5664bf
Sean Kutash <seankutash@gmail.com>
2023-06-26 23:21:46 -0400

    Initialize Vite Project Structure and Install Required Dependencies
    Created the project structure for the ShadowsofEternia application using Vite. The initial file organization includes directories for node_modules, public, and source files (src). Within 'src', subdirectories for assets, components, scenes, and utilities have been created along with essential .tsx and .css files.
    
    Installed required packages listed in the package.json file, which include crucial dependencies like react, react-dom, react-router-dom, redux, and firebase, along with devDependencies such as TypeScript and various ESLint plugins.
    
    Configured Tailwind CSS and PostCSS by setting up their respective configuration files - tailwind.config.json and postcss.config.js - for streamlined CSS styling and transformations.
    
    Configured TypeScript and ESLint rules as per the project requirements and set up the Vite configuration file (vite.config.ts).
    
    This commit marks the foundational setup and configuration for the project, providing a robust starting point for future development.

---
+++ src/Components/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1 className="text-6xl font-extrabold text-indigo-800">
+        This is a sample text to show the website is running and Tailwind is
+        working.
+      </h1>
+    </div>
+  );
+}
+
+export default App;

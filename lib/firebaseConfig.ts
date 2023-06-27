0b131202a00fe1075c13eacd970d59bc2a6e93f6
Sean Kutash <seankutash@gmail.com>
2023-06-27 17:05:36 -0400

    Implement Firebase and connect anonymous sign-in method
    This commit introduces Firebase into the TypeScript React project and establishes a connection to enable anonymous sign-in functionality. The following changes were made:
    
    - Added Firebase SDK to the project dependencies
    - Configured Firebase initialization in the project
    - Created a new utility module for Firebase authentication
    - Implemented anonymous sign-in functionality using Firebase's anonymous authentication method
    - Updated the relevant components to handle the sign-in process
    - Added error handling and feedback for sign-in operations
    
    This addition allows users to sign in anonymously, providing a seamless experience for accessing the application's features without requiring user registration. Anonymous sign-in offers convenience while maintaining privacy.
    
    Please review and test the new implementation.

---
+++ lib/firebaseConfig.ts
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDnDWMeiJpXpwlMSfMDCbiM8hhaojp8J2s",
+  authDomain: "fir-frontend-ece3c.firebaseapp.com",
+  projectId: "fir-frontend-ece3c",
+  storageBucket: "fir-frontend-ece3c.appspot.com",
+  messagingSenderId: "916260554529",
+  appId: "1:916260554529:web:7fd64be9142cbcbc0a2060",
+  measurementId: "G-8QYMCNLQHE",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,10 +1,46 @@
+import React, { useEffect } from "react";
+import { FirebaseApp } from "firebase/app";
+import { app as firebaseApp } from "../../lib/firebaseConfig";
+import { getAuth, signInAnonymously, onAuthStateChanged } from "firebase/auth";
+
+const app: FirebaseApp = firebaseApp;
+const auth = getAuth(app);
+
 function App() {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        console.log("User is signed in", user);
+      } else {
+        console.log("User is signed out");
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  function signInAnon() {
+    signInAnonymously(auth)
+      .then(() => {
+        console.log("Signed in anonymously");
+      })
+      .catch((error: any) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        console.log("Anon sign-in failed!");
+      });
+  }
+
   return (
     <div>
       <h1 className="text-6xl font-extrabold text-indigo-800">
         This is a sample text to show the website is running and Tailwind is
         working.
       </h1>
+      <button onClick={signInAnon}>Sign In Anonymously</button>
     </div>
   );
 }

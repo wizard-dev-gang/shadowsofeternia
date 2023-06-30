c06d9fa48b36d44ea4917c2d9827b0d2b3ac3734
Sean Kutash <seankutash@gmail.com>
2023-06-30 14:47:44 -0400

    Implemented multiplayer first revision. This is really buggy, but it works and is synced in our firebase database.

--- src/Components/GameComponent.tsx
+++ src/Components/GameComponent.tsx
@@ -1,35 +1,45 @@
-import React, { useEffect } from 'react';
-import Phaser from 'phaser';
-import Game from '../scenes/Game';
-import Preloader from '../scenes/Preloader';
+import React, { useEffect } from "react";
+import Phaser from "phaser";
+import Game from "../scenes/Game";
+import Preloader from "../scenes/Preloader";
 
 function GameComponent() {
-    const phaserGameRef = React.useRef(null)
+  const phaserGameRef = React.useRef(null);
 
-    useEffect(() => {
-        if (phaserGameRef.current) {
-            return;
-        }
-        phaserGameRef.current = new Phaser.Game({
-            type: Phaser.AUTO,
-            parent: 'game-content',
-            width: 400,
-            height: 250,
-            physics: {
-                default: 'arcade',
-                arcade: {
-                    gravity: { y: 0},
-                    debug:false
-                },
-            },
-            scene: [Preloader, Game],
-            scale: {
-                zoom:2
-            }
-        });
+  useEffect(() => {
+    if (phaserGameRef.current) {
+      return;
+    }
+    phaserGameRef.current = new Phaser.Game({
+      type: Phaser.AUTO,
+      parent: "game-content",
+      width: 400,
+      height: 250,
+      physics: {
+        default: "arcade",
+        arcade: {
+          gravity: { y: 0 },
+          debug: false,
+        },
+      },
+      scene: [Preloader, Game],
+      scale: {
+        zoom: 2,
+      },
+    });
+  }, []);
 
-    }, []);
-        return <div id='game-content'></div>
+  return (
+    <div
+      id="game-content"
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    ></div>
+  );
 }
 
 export default GameComponent;

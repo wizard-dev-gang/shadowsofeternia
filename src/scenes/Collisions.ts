1c9f4c565c6d885bcd6cd9cdfba86a5fa9f43774
Sean Kutash <seankutash@gmail.com>
2023-07-12 15:29:04 -0400

    Added some changes to Collision Handler

--- src/scenes/Collisions.ts
+++ src/scenes/Collisions.ts
@@ -7,23 +7,28 @@ import { Barb } from "../characters/Barb";
 import "../characters/Archer";
 import { Wizard } from "../characters/Wizard";
 import { Archer } from "../characters/Archer";
+import { Npc_wizard } from "../characters/Npc";
+import "../characters/Npc";
 
 export class CollisionHandler {
   projectiles: Phaser.Physics.Arcade.Group;
   skeletons: Phaser.Physics.Arcade.Group;
   slimes: Phaser.Physics.Arcade.Group;
   time: Phaser.Time.Clock;
+  Npc_wizard!: Phaser.Physics.Arcade.Group;
 
   constructor(
     projectiles: Phaser.Physics.Arcade.Group,
     skeletons: Phaser.Physics.Arcade.Group,
     slimes: Phaser.Physics.Arcade.Group,
-    time: Phaser.Time.Clock
+    time: Phaser.Time.Clock,
+    Npc_wizard: Phaser.Physics.Arcade.Group
   ) {
     this.projectiles = projectiles;
     this.skeletons = skeletons;
     this.slimes = slimes;
     this.time = time;
+    this.Npc_wizard = Npc_wizard;
   }
 
   // Method to handle collision between projectiles and walls
@@ -155,18 +160,46 @@ export class CollisionHandler {
       }
     }
   }
+
   handlePlayerNpcCollision(
     player: Phaser.GameObjects.GameObject,
-    npc: Phaser.GameObjects.GameObject
+    npc: Phaser.GameObjects.GameObject,
   ) {
-    // Check if the player is the wizard character and the NPC is the wizard
+    // Check if the player is interacting with the wizard character
     if (
       player instanceof Player ||
-      Wizard ||
-      (Barb && npc instanceof Npc_wizard)
+      player instanceof Barb ||
+      player instanceof Wizard ||
+      (npc instanceof Npc_wizard && npc instanceof Npc_wizard)
     ) {
       // Perform actions for interacting with the NPC
       console.log("Interacting with the NPC Wizard");
+
+      // Add text on the screen
+      const text = this.add.text(1876, 1028, "Hello World!", {
+        fontSize: "11px",
+        color: "#000000",
+        padding: {
+          left: 10,
+          right: 10,
+          top: 5,
+          bottom: 5,
+        },
+      });
+      text.setOrigin(0.5, 1.4);
+      text.setDepth(1);
+
+      // Add a background image behind the text
+      const background = this.add.image(1876, 1028, "text-bubble");
+      background.setDisplaySize(text.width, text.height);
+      background.setOrigin(0.53, 1.5);
+      background.setDepth(0);
+
+      // Remove the text after a certain delay
+      this.time.delayedCall(2000, () => {
+        text.destroy();
+        background.destroy();
+      });
     }
   }
 }

31bfe4cff6b350201f78842b6c85888e9c23f48d
DMJennings <96453777+DMJennings@users.noreply.github.com>
2023-07-14 14:59:31 -0600

    Update gameOnAuth.ts

--- src/utils/gameOnAuth.ts
+++ src/utils/gameOnAuth.ts
@@ -56,88 +56,88 @@ export const setupFirebaseAuth = (gameInstance: Game) => {
         });
       }
 
-      // onValue(otherPlayersRef, (snapshot) => {
-      //   const playersData = snapshot.val();
-      //   playerScene = playersData[gameInstance.playerId].scene
-      //   gameInstance.otherPlayers.forEach((otherPlayer, playerId) => {
-      //     const playerData = playersData[playerId];
+      onValue(otherPlayersRef, (snapshot) => {
+        const playersData = snapshot.val();
+        playerScene = playersData[gameInstance.playerId].scene
+        gameInstance.otherPlayers.forEach((otherPlayer, playerId) => {
+          const playerData = playersData[playerId];
 
-      //     if (!playerData || !playerData.online) {
-      //       otherPlayer.destroy(); // Remove sprite of other player
-      //       gameInstance.otherPlayers.delete(playerId); // Remove other player from map
+          if (!playerData || !playerData.online) {
+            otherPlayer.destroy(); // Remove sprite of other player
+            gameInstance.otherPlayers.delete(playerId); // Remove other player from map
 
-      //       const playerName = gameInstance.playerNames.get(playerId);
-      //       if (playerName) {
-      //         playerName.destroy(); // Remove player name
-      //         gameInstance.playerNames.delete(playerId); // Remove player name from map
-      //       }
-      //     }
-      //   });
+            const playerName = gameInstance.playerNames.get(playerId);
+            if (playerName) {
+              playerName.destroy(); // Remove player name
+              gameInstance.playerNames.delete(playerId); // Remove player name from map
+            }
+          }
+        });
 
-      //   // Now handle the remaining players in Firebase
-      //   for (const playerId in playersData) {
-      //     if (playerId === gameInstance.playerId) continue; // Skip the current player
+        // Now handle the remaining players in Firebase
+        for (const playerId in playersData) {
+          if (playerId === gameInstance.playerId) continue; // Skip the current player
 
-      //     const playerData = playersData[playerId];
+          const playerData = playersData[playerId];
           
-      //     // Skip if player is not online
-      //     if (!playerData.online) continue;
-      //     //console.log(playerScene,playerData.scene)
-      //     let otherPlayer = gameInstance.otherPlayers.get(playerId);
-      //     if (playerData?.scene === undefined) continue
-      //     if (playerData.scene != playerScene) continue
-      //     // Create or update other players
-      //     if (!otherPlayer) {
-      //       otherPlayer = gameInstance.add.player(
-      //         playerData.x,
-      //         playerData.y,
-      //         "man"
-      //       ); // Create a sprite for the other player
-      //       gameInstance.otherPlayers.set(playerId, otherPlayer);
-      //     }
-      //     otherPlayer.x = playerData.x;
-      //     otherPlayer.y = playerData.y;
+          // Skip if player is not online
+          if (!playerData.online) continue;
+          //console.log(playerScene,playerData.scene)
+          let otherPlayer = gameInstance.otherPlayers.get(playerId);
+          if (playerData?.scene === undefined) continue
+          if (playerData.scene != playerScene) continue
+          // Create or update other players
+          if (!otherPlayer) {
+            otherPlayer = gameInstance.add.player(
+              playerData.x,
+              playerData.y,
+              "man"
+            ); // Create a sprite for the other player
+            gameInstance.otherPlayers.set(playerId, otherPlayer);
+          }
+          otherPlayer.x = playerData.x;
+          otherPlayer.y = playerData.y;
 
-      //     if (playerData.projectilesFromDB)
-      //       otherPlayer.setProjectiles(gameInstance.projectiles);
-      //     for (const projectileId in playerData.projectilesFromDB) {
-      //       const projectileData = playerData.projectilesFromDB[projectileId];
-      //       otherPlayer.throwProjectile(
-      //         projectileData.direction,
-      //         projectileData.x,
-      //         projectileData.y,
-      //         projectileData.attackObj
-      //       );
-      //     }
+          if (playerData.projectilesFromDB)
+            otherPlayer.setProjectiles(gameInstance.projectiles);
+          for (const projectileId in playerData.projectilesFromDB) {
+            const projectileData = playerData.projectilesFromDB[projectileId];
+            otherPlayer.throwProjectile(
+              projectileData.direction,
+              projectileData.x,
+              projectileData.y,
+              projectileData.attackObj
+            );
+          }
 
-      //     // Play animation and set frame for other players
-      //     if (playerData.anim && playerData.frame) {
-      //       otherPlayer.anims.play(playerData.anim, true);
-      //       otherPlayer.anims.setCurrentFrame(
-      //         otherPlayer.anims.currentAnim.frames.find(
-      //           (f: any) => f.frame.name === playerData.frame
-      //         )
-      //       );
-      //     }
+          // Play animation and set frame for other players
+          if (playerData.anim && playerData.frame) {
+            otherPlayer.anims.play(playerData.anim, true);
+            otherPlayer.anims.setCurrentFrame(
+              otherPlayer.anims.currentAnim.frames.find(
+                (f: any) => f.frame.name === playerData.frame
+              )
+            );
+          }
 
-      //     // Create or update player names
-      //     let playerName = gameInstance.playerNames.get(playerId);
-      //     if (!playerName) {
-      //       playerName = gameInstance.add
-      //         .text(0, 0, playerData.name, {
-      //           fontSize: "10px",
-      //           color: "#ffffff",
-      //           stroke: "#000000",
-      //           strokeThickness: 2,
-      //         })
-      //         .setOrigin(0.5, 0.01);
-      //       gameInstance.playerNames.set(playerId, playerName);
-      //     }
-      //     // Player name position
-      //     playerName.x = otherPlayer.x;
-      //     playerName.y = otherPlayer.y - 20;
-      //   }
-      // });
+          // Create or update player names
+          let playerName = gameInstance.playerNames.get(playerId);
+          if (!playerName) {
+            playerName = gameInstance.add
+              .text(0, 0, playerData.name, {
+                fontSize: "10px",
+                color: "#ffffff",
+                stroke: "#000000",
+                strokeThickness: 2,
+              })
+              .setOrigin(0.5, 0.01);
+            gameInstance.playerNames.set(playerId, playerName);
+          }
+          // Player name position
+          playerName.x = otherPlayer.x;
+          playerName.y = otherPlayer.y - 20;
+        }
+      });
       onValue(gameInstance.playerRef, (snapshot) => {
         const playerData = snapshot.val();
         if (playerData && playerData.exp) {

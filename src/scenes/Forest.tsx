80e01ca72437d259f3fc7cda745b1748bb4cc2e9
HRM0 <103372533+HRM0@users.noreply.github.com>
2023-07-17 17:45:02 -0400

    removed legacy forest transition code

--- src/scenes/Forest.tsx
+++ src/scenes/Forest.tsx
@@ -522,15 +522,6 @@ export default class Forest extends Phaser.Scene {
 
     this.enemiesSpawned = true;
 
-    const forestX = character.x >= 709 && character.x <= 825;
-    const forestY = character.y <= 3152 && character.y >= 3140;
-    if (forestX && forestY) {
-      if (this.game) this.game.sceneFrom = "forest";
-      this.scene.switch("game");
-      // this.scene.get("game").events.emit("spawnAtEntrance", 2070, 29);
-      return;
-    }
-
     const ruinsX = character.x >= 647 && character.x <= 990;
     const ruinsY = character.y <= 35 && character.y >= 27;
     if (ruinsX && ruinsY) {
